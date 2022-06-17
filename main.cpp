ba29cc0324b671960a8637d1fa0c6cfc57f37487
James <jjcastle@utexas.edu>
2022-06-16 20:47:46 -0500

    sockMerchant: Add new pseudocode to describe different way to approach problem with index-matched vectors.

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ string rtrim(const string &);
 vector<string> split(const string &);
 
 /*
- * Complete the 'sockMerchant' function below.
+ * Complete the 'sockMerchant' function below. Each unique int in the array is a "Color" of sock.
  *
  * The function is expected to return an INTEGER.
  * The function accepts following parameters:
@@ -18,23 +18,44 @@ vector<string> split(const string &);
 int sockMerchant(int totalSocks, vector<int> arrayOfSocks) {
     int pairsOfSocks = 0;
 
-    vector <vector<int>> uniqueValueArray;
+    vector <int> uniqueValueArray;
+    vector <int> valueDuplicateCounter;
     //need an algorithm that looks at numbers in an array for pairs (divisible by 2... so count up unique instances
     //of a number and if %2, then a pair. If not % 2, then subtract 1, and add it to the odd "sock pile".
 
+    //What if I make an index-matched array that corresponds to the unique values array, but captures the count of
+    //each element in the unique array.
+    for (int i = 0; i < arrayOfSocks.size(); i++) {
+        int currentColor = 0;
+        //Search for current "Color" in my uniqueValueArray
+        //If the value is not found in the unique array, pushback the currentColor into the uniqueArray.
+        //At the same time, pushback '1' into the duplicateValueCounter at the same index as the uniqueArray.
+
+        //If the value is already in the uniqueValueArray, then take the current index, find the value currently
+        //in the duplicateValueCounter Array, and add 1.
+    }
+
+    //Step through the duplicateValueCounter
+    // % each element by 2. If 0, then take the value in the element, and divide by 2. Add this value to
+    //pairsOfSOcks.
+
+    //if % not 0, then increment lonelySockCount, subtract 1 from current element value. % 2. If 0, then
+    // divide value by 2 as before, and add this amount to pairsOfSocks.
+    
+    //Return the final number of pairs of socks. Do nothing with loaner sock count for now...
+    return pairsOfSocks;
+    //Original Idea:
     //Consider making a vector of vectors of ints that holds unique values, ands then the count of duplicates will
     // be in the vector that comes off of each one. Then can call the size function on the sub-vector to determine
     // number of duplicates.
-
+    //Iterate through array of socks
+    //Check if uniqueValueArray has a vector in it that houses our the targetNumber.
     //Check each element of the unique value array, and do a size of the vector within.
     // Take this size, and add 1 for the unique value. % 2 this value.
     //If  % = 0, then divide total by 2 to and add this to the pairCounter.
     //If % != 1, then add one to lonelySockCount, subtract 1 amount of socks in the color, and check % 2 = 0.
     //If now 0, then use logic for  % 0.
     //This value should give you the number of pairs remaining. Add this value to the pair counter.
-
-    //Return the final number of pairs of socks. Do nothing with loaner sock count for now...
-    return pairsOfSocks;
 }
 
 int main()
